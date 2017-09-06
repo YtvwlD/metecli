fe0bb78ebbf0dbfb8cd8952ccbb43ae5c7144e74
Niklas Sombert <niklas@ytvwld.de>
2017-09-06 16:51:00 +0200

    Only instantiate Account if neccessary.

--- metecli/account.py
+++ metecli/account.py
@@ -10,34 +10,33 @@ def do(args):
     print("Please select an action (see '--help').")
 
 def setup_cmdline(global_subparsers):
-    acc = Account()
     parser = global_subparsers.add_parser("account", help="show or modify an account")
     subparsers = parser.add_subparsers(help="action")
     parser_buy = subparsers.add_parser("buy", help="buys a drink")
     parser_buy.add_argument("drink", type=str, help="the drink to buy")
-    parser_buy.set_defaults(func=acc.buy)
+    parser_buy.set_defaults(func=lambda args: Account().buy(args))
     parser_buy_barcode = subparsers.add_parser("buy_barcode", help="buys a drink by barcode")
     parser_buy_barcode.add_argument("barcode", type=int, help="the barcode")
-    parser_buy_barcode.set_defaults(func=acc.buy_barcode)
+    parser_buy_barcode.set_defaults(func=lambda args: Account().buy_barcode(args))
     parser_pay = subparsers.add_parser("pay", help="subtracts an amount from your balance")
     parser_pay.add_argument("amount", type=float, help="the amount to subtract")
-    parser_pay.set_defaults(func=acc.pay)
+    parser_pay.set_defaults(func=lambda args: Account().pay(args))
     parser_deposit = subparsers.add_parser("deposit", help="add an amount to your balance")
     parser_deposit.add_argument("amount", type=float, help="the amount to add")
-    parser_deposit.set_defaults(func=acc.deposit)
+    parser_deposit.set_defaults(func=lambda args: Account().deposit(args))
     parser_logs = subparsers.add_parser("logs", help="display the logs")
-    parser_logs.set_defaults(func=acc.logs)
+    parser_logs.set_defaults(func=lambda args: Account().logs(args))
     parser.set_defaults(func=do)
 
 class Account():
     def __init__(self):
         config = Config()
         if not "base_url" in config.settings["connection"]:
-            log.warn("Connection is not configured yet. Account management won't be possible.")
+            raise Exception("Connection is not configured yet. Account management isn't possible.")
         else:
             self._conn = Connection(base_url=config.settings["connection"]["base_url"])
         if not "uid" in config.settings["connection"]:
-            log.warn("User account is not configured yet. Account management won't be possible.")
+            raise Exception("User account is not configured yet. Account management isn't possible.")
         else:
             self._uid = config.settings["connection"]["uid"]
     
