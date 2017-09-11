df9891d252625e05ba9d4f6d6f37726d42279741
Niklas Sombert <niklas@ytvwld.de>
2017-09-11 10:19:02 +0200

    Display a specific drink.

--- metecli/drinks.py
+++ metecli/drinks.py
@@ -1,16 +1,19 @@
 from .connection import Connection
-from .utils import true_false_to_yes_no
+from .utils import true_false_to_yes_no, fuzzy_search
 
 from tabulate import tabulate
 
 def setup_cmdline(global_subparsers):
     parser = global_subparsers.add_parser("drinks", help="show or modify drinks")
     subparsers = parser.add_subparsers(help="action")
-    parser_show = subparsers.add_parser("show", help="lists all drinks")
+    parser_list = subparsers.add_parser("list", help="lists all drinks")
+    parser_list.set_defaults(func=list_drinks)
+    parser_show = subparsers.add_parser("show", help="display detailed information about a drink")
+    parser_show.add_argument("drink", help="the drink to display")
     parser_show.set_defaults(func=show)
-    parser.set_defaults(func=show)
+    parser.set_defaults(func=list_drinks)
 
-def show(args, config):
+def list_drinks(args, config):
     conn = Connection(base_url=config.settings["connection"]["base_url"])
     drinks = conn.drinks()
     print("All drinks:")
@@ -32,4 +35,21 @@ def show(args, config):
             "active?"
         ],
         tablefmt=config.settings["display"]["table_format"],
+    ))
+
+def show(args, config):
+    conn = Connection(base_url=config.settings["connection"]["base_url"])
+    drink = fuzzy_search(conn, "drink", args.drink)
+    if not drink:
+        return
+    print(tabulate(
+        [
+            ["ID", drink["id"]],
+            ["name", drink["name"]],
+            ["price", "{:.2f} €".format(float(drink["price"]))],
+            ["bottle size", drink["bottle_size"]],
+            ["caffeine", drink["caffeine"]],
+            ["active?", true_false_to_yes_no(drink["active"])],
+        ],
+        tablefmt=config.settings["display"]["table_format"]
     ))
