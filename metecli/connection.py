a862905701eaa54570b5709b7ca5adb7f344608d
Niklas Sombert <niklas@ytvwld.de>
2017-10-03 11:46:37 +0200

    Connection: Use a Session.
    This can speed up some requests.

--- metecli/connection.py
+++ metecli/connection.py
@@ -1,4 +1,4 @@
-import requests
+from requests import Session
 from urllib.parse import urljoin
 
 import logging
@@ -7,10 +7,11 @@ log = logging.getLogger(__name__)
 class Connection():
     def __init__(self, base_url=None):
         self._base_url = base_url
+        self._sess = Session()
     
     def users(self):
         """Lists all users."""
-        r = requests.get(urljoin(self._base_url, "users.json"))
+        r = self._sess.get(urljoin(self._base_url, "users.json"))
         assert r.ok
         return r.json()
     
@@ -28,102 +29,102 @@ class Connection():
             params["end_date[year]"] = to_date.year
             params["end_date[month]"] = to_date.month
             params["end_date[day]"] = to_date.day
-        r = requests.get(urljoin(self._base_url, "audits.json"), params=params)
+        r = self._sess.get(urljoin(self._base_url, "audits.json"), params=params)
         assert r.ok
         return r.json()
     
     def get_user(self, uid):
         """Get information about a user."""
-        r = requests.get(urljoin(self._base_url, "users/{}.json".format(uid)))
+        r = self._sess.get(urljoin(self._base_url, "users/{}.json".format(uid)))
         assert r.ok
         return r.json()
     
     def modify_user(self, user):
         """Modifys an existing user."""
-        r = requests.patch(urljoin(self._base_url, "users/{}.json").format(user["id"]), json=user)
+        r = self._sess.patch(urljoin(self._base_url, "users/{}.json").format(user["id"]), json=user)
         assert r.ok
     
     def delete_user(self, uid):
-        r = requests.delete(urljoin(self._base_url, "users/{}.json".format(uid)))
+        r = self._sess.delete(urljoin(self._base_url, "users/{}.json".format(uid)))
         assert r.ok
     
     def get_user_defaults(self):
         """Gets the default settings for creating a new user."""
-        r = requests.get(urljoin(self._base_url, "users/new.json"))
+        r = self._sess.get(urljoin(self._base_url, "users/new.json"))
         assert r.ok
         return r.json()
     
     def add_user(self, user):
         """Creates a new user."""
-        r = requests.post(urljoin(self._base_url, "users.json"), json=user)
+        r = self._sess.post(urljoin(self._base_url, "users.json"), json=user)
         assert r.ok
         return r.json()
     
     def buy(self, uid, did):
         """Buy a drink."""
-        r = requests.get(urljoin(self._base_url, "users/{}/buy.json?drink={}".format(uid, did)))
+        r = self._sess.get(urljoin(self._base_url, "users/{}/buy.json?drink={}".format(uid, did)))
         assert r.ok
     
     def pay(self, uid, amount):
         """Pay an amount."""
-        r = requests.get(urljoin(self._base_url, "users/{}/payment.json?amount={}".format(uid, amount)))
+        r = self._sess.get(urljoin(self._base_url, "users/{}/payment.json?amount={}".format(uid, amount)))
         print(r.text)
         assert r.ok
     
     def deposit(self, uid, amount):
         """Deposit money."""
-        r = requests.get(urljoin(self._base_url, "users/{}/deposit.json?amount={}".format(uid, amount)))
+        r = self._sess.get(urljoin(self._base_url, "users/{}/deposit.json?amount={}".format(uid, amount)))
         print(r.text)
         assert r.ok
     
     def drinks(self):
         """Lists all drinks."""
-        r = requests.get(urljoin(self._base_url, "drinks.json"))
+        r = self._sess.get(urljoin(self._base_url, "drinks.json"))
         assert r.ok
         return r.json()
     
     def modify_drink(self, drink):
         """Modifys an existing drink."""
-        r = requests.patch(urljoin(self._base_url, "drinks/{}.json").format(drink["id"]), json=drink)
+        r = self._sess.patch(urljoin(self._base_url, "drinks/{}.json").format(drink["id"]), json=drink)
         assert r.ok
     
     def get_drink_defaults(self):
         """Gets the default settings for creating a new drink."""
-        r = requests.get(urljoin(self._base_url, "drinks/new.json"))
+        r = self._sess.get(urljoin(self._base_url, "drinks/new.json"))
         assert r.ok
         return r.json()
     
     def create_drink(self, drink):
         """Creates a new drink."""
-        r = requests.post(urljoin(self._base_url, "drinks.json"), json=drink)
+        r = self._sess.post(urljoin(self._base_url, "drinks.json"), json=drink)
         assert r.ok
         return r.json()
     
     def delete_drink(self, drink_id):
         """Deletes an existing drink."""
-        r = requests.delete(urljoin(self._base_url, "drinks/{}.json").format(drink_id))
+        r = self._sess.delete(urljoin(self._base_url, "drinks/{}.json").format(drink_id))
         assert r.ok
     
     def barcodes(self):
         """Lists all barcodes."""
-        r = requests.get(urljoin(self._base_url, "barcodes.json"))
+        r = self._sess.get(urljoin(self._base_url, "barcodes.json"))
         return r.json()
     
     def get_barcode_defaults(self):
         """Get the defaults for creating new barcodes."""
-        r = requests.get(urljoin(self._base_url, "barcodes/new.json"))
+        r = self._sess.get(urljoin(self._base_url, "barcodes/new.json"))
         assert r.ok
         return r.json()
     
     def create_barcode(self, barcode):
         """Creates a new barcode."""
-        r = requests.post(urljoin(self._base_url, "barcodes.json"), json=barcode)
+        r = self._sess.post(urljoin(self._base_url, "barcodes.json"), json=barcode)
         assert r.ok
         return r.json()
     
     def delete_barcode(self, barcode_id):
         """Delete a barcode."""
-        r = requests.delete(urljoin(self._base_url, "barcodes/{}.json").format(barcode_id))
+        r = self._sess.delete(urljoin(self._base_url, "barcodes/{}.json").format(barcode_id))
         assert r.ok
     
     def try_connect(self):
