4a083e504da041dc74f38d3d06f8b9e8078430d5
Niklas Sombert <niklas@ytvwld.de>
2017-10-06 12:35:47 +0200

    Connection: Try to upgrade the API version.

--- metecli/connection.py
+++ metecli/connection.py
@@ -16,6 +16,7 @@ class Connection():
                 raise Exception("The configured connection doesn't have api_version set.")
             self._api_version = config["connection"]["api_version"]
             assert self._api_version in ("legacy", "v1")
+            self._try_upgrade()
         elif base_url and not config:
             self._conf = None
             self._base_url = base_url
@@ -156,4 +157,30 @@ class Connection():
             return "v1"
         else:
             return "legacy"
-        
+    
+    def _try_upgrade(self):
+        """Tries to upgrade the API version."""
+        changed = False
+        for upgrade in (("legacy", "v1"),): # What should be upgraded?
+            if self._api_version != upgrade[0]:
+                continue
+            log.info("Trying to upgrade the API version from '%s' to '%s'...", upgrade[0], upgrade[1])
+            # save the old values
+            old_base_url = self._base_url
+            self._api_version = upgrade[1]
+            try:
+                if upgrade == ("legacy", "v1"): # try legacy -> v1
+                    self._base_url = urljoin(self._base_url, "api/v1/")
+                    assert self.try_connect()
+                    changed = True
+            except:
+                # something went wrong
+                log.warning("The server doesn't support API version '%s'. (Or the connection failed.)", upgrade[1])
+                # restore the old values
+                self._api_version = upgrade[0]
+                self._base_url = old_base_url
+        if changed and self._conf:
+            # save the new values
+            self._conf["connection"]["api_version"] == self._api_version
+            self._conf["connection"]["base_url"] == self._base_url
+            self._conf.save()
