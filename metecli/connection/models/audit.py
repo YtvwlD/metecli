0db9cd30647c7393915b7802d4a6f9f34adbfe6c
Niklas Sombert <niklas@ytvwld.de>
2021-04-01 19:44:15 +0200

    Implement from_v2 and to_v2 for the models

--- metecli/connection/models/audit.py
+++ metecli/connection/models/audit.py
@@ -16,6 +16,16 @@ class Audit:
             drink=int(data["drink"]) if data["drink"] is not None else None,
         )
     
+    @classmethod
+    def from_v2(cls, data: Dict[str, Any]) -> 'Audit':
+        return cls(
+            id=int(data["id"]),
+            # TODO: turn this into datetime?
+            created_at=str(data["created_at"]),
+            difference=int(data["difference"]) / 100,
+            drink=int(data["product"]) if data["product"] is not None else None,
+        )
+    
     def __repr__(self) -> str:
         return "Audit({})".format(
             ",".join(["{}={}".format(*item) for item in vars(self).items()])
@@ -33,6 +43,15 @@ class AuditInfo:
             payments_sum=float(data["payments_sum"]),
             deposits_sum=float(data["deposits_sum"]),
             audits=[Audit.from_v1(a) for a in data["audits"]],
+        )
+    
+    @classmethod
+    def from_v2(cls, data: Dict[str, Any]) -> 'AuditInfo':
+        return cls(
+            sum=int(data["sum"]) / 100,
+            payments_sum=int(data["payments_sum"]) / 100,
+            deposits_sum=int(data["deposits_sum"]) / 100,
+            audits=[Audit.from_v2(a) for a in data["audits"]],
         )
     
     def __repr__(self) -> str:
